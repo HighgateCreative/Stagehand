e17a223e2eac6b9b951bc282caa533557e211062
Sean Zellmer <sean@lejeunerenard.com>
2013-03-12 09:48:10 -0500

    Added Upload file function

--- Stagehand.pm
+++ Stagehand.pm
@@ -6,10 +6,11 @@ use vars qw($VERSION @ISA @EXPORT);
 
 $VERSION = 1.00;
 @ISA = qw(Exporter);
-@EXPORT = qw(model fillinform);
+@EXPORT = qw(model fillinform upload_file);
 
 use Dancer ':syntax';
 use Dancer::Plugin::DBIC 'schema';
+use Data::Dumper;
 
 sub model {
    return schema->resultset( shift );
@@ -21,6 +22,53 @@ sub fillinform {
    my $fifvalues = shift;
    my $html = template $template, $fifvalues;
    return HTML::FillInForm->fill( \$html, $fifvalues );
+}
+
+sub upload_file {
+   $| = 1;
+
+   my ($upload, $max, $upload_dir, $sub_dir, $filename, $overwrite) = @_; #upload is the $query->param
+
+   if (not defined $overwrite) { # Set default to 0
+      $overwrite = 1;
+   }
+                                                              #upload_is the main path, $sub_dir is an option new directory that will 
+                                                              # that will be created below and appended to $updir                                                            
+   $filename =~ s/ /_/g;
+   $filename =~ s/^.*[\/ | \\](.*)$/$1/; #strip off path
+
+   if (! -e $upload_dir) { return( undef, "$upload_dir does not exist", undef) };
+
+   #my $upload_filehandle = $self->query->upload($upload);
+   if (!$upload) { return ($filename,'Bad file handle. Check form enctype.',$upload); }
+
+   if ($upload->size > $max) {
+      return ($filename,'File is too large to upload.',$upload->size);
+   }
+
+   if( $sub_dir && ! -e "$upload_dir/$sub_dir") {
+      mkdir ("$upload_dir/$sub_dir") or return ($sub_dir, $!, undef);
+      my $mode = 0770;   chmod $mode, "$upload_dir/$sub_dir";
+      chown 33, 1001, "$upload_dir/$sub_dir";
+   }
+
+   $upload_dir = ( $sub_dir ) ? $upload_dir .'/'.$sub_dir : $upload_dir;
+
+   if ( ( not $overwrite ) and -e "$upload_dir/$filename") {
+      return ($filename,'File already exists. Rename and try again.',$upload->size);
+   } elsif ($overwrite and -e "$upload_dir/$filename") {  #delete file first if one by same name exists
+      unlink "$upload_dir/$filename";
+   }
+
+   if (not $upload->copy_to($upload_dir."/".$filename)) {
+      return ($filename, 'File failed to copy to '.$upload_dir, $upload->size);
+   }
+
+   #my $mode = ($upload_dir =~ /client_documents|client_images/) ? 0770 : 0777;   
+   #chmod 0770, "$upload_dir/$filename" or return ($filename, 'chmod failed', $upload->size);
+   #chown 33, 1001, "$upload_dir/$sub_dir" or return ($filename, 'chown failed', $upload->size);
+
+   return ($filename, '', $upload->size);
 }
 
  1;
